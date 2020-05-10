a63b4b63ed24157d24fcef928eaf11ec1ae76d10
Pradeep Suthar <49487927+sutharp777@users.noreply.github.com>
2020-05-10 22:10:06 +0530

    Update reader_writer_multithread.c

--- reader_writer_multithread.c
+++ reader_writer_multithread.c
@@ -112,7 +112,7 @@ int main()
          
     }
 
-    for (int i = 0; i < 5; i++) 
+    for (int i=0; i<5; i++) 
     {
         pthread_create( &writer[i], NULL, write_func, (void*)&a[i]);
         pthread_setschedprio( writer[i], 2);
@@ -124,7 +124,7 @@ int main()
 
     }
 
-    for (int i = 0; i < 5; i++) 
+    for (int i=0; i<5; i++) 
     {
         pthread_join(reader[i], NULL);
     }
