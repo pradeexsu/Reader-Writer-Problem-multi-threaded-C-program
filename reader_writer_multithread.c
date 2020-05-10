a86e65ff573f5fdc7fcc6b23f7b7d9f96c82fe1d
Pradeep Suthar <49487927+sutharp777@users.noreply.github.com>
2020-05-10 21:59:25 +0530

    Update reader_writer_multithread.c

--- reader_writer_multithread.c
+++ reader_writer_multithread.c
@@ -31,7 +31,7 @@ struct var global_var = {"default", 50};
 
 int xyz = 10;    // another global variable
 int a[5]={1,2,3,4,5};
-char string[][10]={"","one","two","three","four","five"};
+char string[][10] = {"","one","two","three","four","five"};
 
 void *read_func(void *args){
 
